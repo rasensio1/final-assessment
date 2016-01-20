08af872d89adde5552bdaa73664f910ecdca705b
Ryan Asensio <ryan.asensio@gmail.com>
2016-01-20 10:33:25 -0700

    can delete list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,16 @@
 class ListsController < ApplicationController
   def create
-    user = List.new(list_params)
+    List.create(list_params)
+    redirect_to :back
+  end
+
+  def destroy
+    List.delete(params["id"])
     redirect_to :back
   end
 
   private
     def list_params
-      params.require(:list).permit(:title, :description)
+      params.require(:list).permit(:title, :description, :user_id)
     end
 end
